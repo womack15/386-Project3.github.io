9e0b72cfddfd90bbc9cc4eecf265c8e82d2e1e2d
Josh Womack <jdwomack@csu.fullerton.edu>
2017-05-07 19:33:50 -0700

    first commit

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1494210792,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/rockground.png",
+		"images/dirtpatch-sheet0.png",
+		"images/acorn-sheet0.png",
+		"images/dirtwall-sheet0.png",
+		"images/gopher-sheet0.png",
+		"images/background.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/enemysnake-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
